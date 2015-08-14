b523d603f293da0d54d805104466bd616f4269ab
Nate Reinhardt <natemreinhardt@gmail.com>
2015-08-13 23:56:30 -0500

    Successfully got single page working and routes

---
+++ client/js/controllers/dashboardCtrl.js
@@ -0,0 +1,13 @@
+(function (angular){
+    'use strict';
+    var ngModule = angular.module('eos.dashboardCtrl',[]);
+
+    ngModule.controller('dashboardCtrl', function ($scope){
+        $scope.welcome = 'Welcome To The Dashboard!!!'
+
+
+    })
+
+}(window.angular));
+
+
--- server.js
+++ server.js
@@ -2,23 +2,38 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
-    opsController = require('./server/controllers/opsController');
+    methodOverride = require('method-override');
+
 
 mongoose.connect('mongodb://localhost:27017/eos');
-app.use(bodyParser());
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/client/views/index.html');
-});
+
+// get all data/stuff of the body (POST) parameters
+app.use(bodyParser.json());
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+app.use(methodOverride('X-HTTP-Method-Override'));
 app.use('/js', express.static(__dirname + '/client/js'));
-
+app.use('/views', express.static(__dirname + '/client/views'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
-
-app.listen(3000, function () {
-    console.log('Im Listening...');
-
-    app.get('/api/ops', opsController.list);
-    app.post('/api/ops', opsController.create);
+app.use('/node_modules', express.static(__dirname +'/node_modules'));
 
 
-});
+require('./server/routes')(app);
+app.listen(3000);
+console.log('Im Listening...');
+exports = module.exports = app;
+
+
+//app.get('/', function (req, res) {
+//    res.sendFile(__dirname + '/client/views/index.html');
+//});
+//app.get('*', function (req, res) {
+//    res.sendFile(__dirname + '/client/views/index.html');
+//});

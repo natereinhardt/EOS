c40e527a815ed4446ba4e4e0b599e7e11a997e75
natereinhardt <natemreinhardt@gmail.com>
2015-08-19 21:58:30 -0500

    commiting to master like a bawz

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,11 +67,8 @@ module.exports = function(grunt) {
     // we can only load these if they are in our package.json
     // make sure you have run npm install so our app can find these
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-ngmin');
+    
 
 };

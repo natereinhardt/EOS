23f07b9e07956b375a255624e2da33bc497ab43f
Intern Nate <nate.reinhardt@wwt.com>
2016-02-10 15:48:36 -0600

    fleshing out an item

--- server/item/itemCtrl.js
+++ server/item/itemCtrl.js
@@ -1,3 +1,16 @@
-/**
- * Created by reinharn on 2/10/16.
- */
+var Item = require('./item');
+
+module.exports.create = function (req, res) {
+
+    var item = new Item();
+    item.name = req.params;
+
+    item.save(function (err, result) {
+        res.json(result);
+    });
+};
+module.exports.list = function (req, res) {
+    Item.find({}, function (err, results) {
+        res.json(results);
+    });
+};
